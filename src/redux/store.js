d0c721bde98e002de47491a63f058d0f51e517c4
tommydemarco <tommaso.demarco95@gmail.com>
2020-11-06 22:48:11 +0000

    Product Item styles fixed, redux store bug fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 
-import rootReducer from './root-reducer';
+import rootReduceriuuuu from './root-reducer';
 
-const middlewares = [logger]
+const middlewares = [ logger ]
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore( rootReduceriuuuu, applyMiddleware( ...middlewares ) )
 
 //a persisted version of the store for LOCAL STORAGE 
-export const persistor = persistStore(store)
+export const persistor = persistStore( store )
