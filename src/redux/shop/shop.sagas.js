2b43afe3e992cd52f89e2c631c38d61ee98a0108
tommydemarco <tommaso.demarco95gmail.com>
2020-12-19 11:42:24 +0100

    Deeper dive into sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,27 +1,34 @@
-import { takeEvery, call, put } from 'redux-saga/effects'
+//===> TAKEEVERY: listens to actions of  specific type
+/*===> CALL: a function that invokes a function (first argument)
+             with the arguments of the functon to invoke (second argument)
+*/
+import { takeEvery, call, put } from "redux-saga/effects";
 
-import { firestore, convertCollectionArrayToMap } from '../../firebase/firebase.utils'
+import {
+  firestore,
+  convertCollectionArrayToMap,
+} from "../../firebase/firebase.utils";
 
-import { fetchCollectionSuccess, fetchCollectionError } from './shop.actions'
+import { fetchCollectionSuccess, fetchCollectionError } from "./shop.actions";
 
-import ShopTypes from './shop.types'
+import ShopTypes from "./shop.types";
 
-//creating the generator functions that fire off during the yelds 
+//creating the generator functions that fire off during the yelds
 export function* fetchCollectionAsync() {
-    try {
-        const collectionRef = firestore.collection( 'collections' )
-        const snapshot = yield collectionRef.get()
-            //using the call effect is optional but is recommended 
-        const collectionsMap = yield call( convertCollectionArrayToMap, snapshot )
-            //put is the equivalent of dispatch for sagas, we are dispatching 
-            //the action creator
-        yield put( fetchCollectionSuccess( collectionsMap ) )
-    } catch( e ) {
-        yield put( fetchCollectionError( e.message ) )
-    }
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    //using the call effect is optional but is recommended
+    const collectionsMap = yield call(convertCollectionArrayToMap, snapshot);
+    //put is the equivalent of dispatch for sagas, we are dispatching
+    //the action creator
+    yield put(fetchCollectionSuccess(collectionsMap));
+  } catch (e) {
+    yield put(fetchCollectionError(e.message));
+  }
 }
 
 //creating the main generator function
 export function* fetchCollectionStart() {
-    yield takeEvery( ShopTypes.FETCH_COLLECTION_START, fetchCollectionAsync )
+  yield takeEvery(ShopTypes.FETCH_COLLECTION_START, fetchCollectionAsync);
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from "redux";
 
 //THUNK - can be replaced by SAGA
 //import thunk from 'redux-thunk'
-import { persistStore } from 'redux-persist';
-import rootReducer from './root-reducer';
+import { persistStore } from "redux-persist";
+import rootReducer from "./root-reducer";
 
-import createSagaMiddleware from 'redux-saga'
-import { fetchCollectionStart } from './shop/shop.sagas'
+//===> IMPORTING THE NECESSARY SAGA FUNCTION FROM REDUX SAGAS
+import createSagaMiddleware from "redux-saga";
+import { fetchCollectionStart } from "./shop/shop.sagas";
 
-//Redux browser extension
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+//===> CREATING AN INSTANCE OF THE SAGA MIDDLEWARE
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [ sagaMiddleware ] //[ logger ]
+//===> INSERTIN SAGAS INTO THE ARRAY OF MIDDLEWARES
+const middlewares = [sagaMiddleware];
 
-export const store = createStore( rootReducer, composeEnhancers( applyMiddleware( ...middlewares ) ) )
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
-sagaMiddleware.run( fetchCollectionStart )
+//===> RUNNING THE CREATED SAGAS
+sagaMiddleware.run(fetchCollectionStart);
 
-//a persisted version of the store for LOCAL STORAGE 
-export const persistor = persistStore( store )
+//a persisted version of the store for LOCAL STORAGE
+export const persistor = persistStore(store);
