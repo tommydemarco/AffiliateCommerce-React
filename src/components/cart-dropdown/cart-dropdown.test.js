76ceb0eaeb40dad4b0d246a87099ef7f5e769e67
tommydemarco <tommaso.demarco95@gmail.com>
2021-01-19 23:48:26 +0100

    basic test added for CartDropdown component

---
+++ src/components/cart-dropdown/cart-dropdown.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+import CartDropdown from "./CartDropdown";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders the component without crashing", () => {
+  const wrapper = shallow(<CartDropdown />);
+});
